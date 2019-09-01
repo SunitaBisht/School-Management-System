f36ab98e8c4ef51e964aa861ae755a695a81ed66
Sunita bisht <49061482+SunitaBisht@users.noreply.github.com>
2019-09-01 21:44:48 +0530

    User Details display

---
+++ ShowUserDetail.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace SchoolManagement
+{
+    public partial class ShowUserDetail : Form
+    {
+        public ShowUserDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mycon ob = new mycon();
+            OleDbConnection con = ob.conn();
+            String sqlcmd = " Select * from CreateAccount";
+           // OleDbDataReader dr = ob.getTable(sqlcmd, con);
+            dataGridView1.DataSource = ob.getTable(sqlcmd, con);
+            //dataGridView1.da
+            // dr.Close();
+            con.Close();
+        }
+    }
+}
