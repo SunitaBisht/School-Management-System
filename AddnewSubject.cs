2b9aa3cf9c7224d3b60107677025143695661f8f
Sunita bisht <49061482+SunitaBisht@users.noreply.github.com>
2019-09-01 21:10:08 +0530

    new subject
    Add New Subjects

---
+++ AddnewSubject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace SchoolManagement
+{
+    public partial class AddnewSubject : Form
+    {
+        public AddnewSubject()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" || textBox1.Text == "" || textBox2.Text == "")
+            {
+                MessageBox.Show("please fill all details");
+            }
+            else
+            {
+                mycon ob = new mycon();
+                OleDbConnection con = ob.conn();
+                String sqlcmd = "Insert into Subject values('" + comboBox1.Text + "','" + textBox1.Text + "','" + textBox2.Text + "')";
+
+                int n = ob.putData(sqlcmd, con);
+                if (n >= 1)
+                {
+                    MessageBox.Show("Data save sucessfully");
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
