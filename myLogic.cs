669213120294fe681720aad2416e03c78c1b0825
Sunita bisht <49061482+SunitaBisht@users.noreply.github.com>
2019-09-01 21:40:01 +0530

    connections
    Connections of pages get and put data

---
+++ myLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement
+{
+    class myLogic
+    {
+        public static String getid()
+        {
+            String c_data = DateTime.Now.ToString();
+            String id = "";
+            foreach (char ch in c_data)
+            {
+                if (ch >= '0' && ch <= '9')
+                {
+                    id = id + ch;
+                }
+            }
+            if (id.Length == 12)
+            {
+                id = id.Remove(3, 4);
+            }
+            else if (id.Length == 13)
+            {
+                id = id.Remove(4, 4);
+            }
+            return (id);
+        }
+        public static string getRandom()
+        {
+            Random rd = new Random();
+
+            string r_num = rd.Next(1, 5000).ToString();
+            return (r_num);
+        }
+    }
+}
---
+++ mycon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace SchoolManagement
+{
+    class mycon
+    {
+        public OleDbConnection conn()
+        {
+            OleDbConnection con = new OleDbConnection("provider = Microsoft.Jet.OLEDB.4.0; Data Source=E:/ssm/ssm.mdb");       //fixed code or location name(path)
+           con.Open();
+            return con;
+        }
+        public int putData(String sqlcmd, OleDbConnection con)                               //put data create code se    //data le rha hai
+        {
+            OleDbCommand com = new OleDbCommand(sqlcmd, con);
+            int n = com.ExecuteNonQuery();                                                    //use executenonquery means change table data
+            return n;
+        }
+        public OleDbDataReader getData(String sqlcmd, OleDbConnection con)               
+        {                                                                                           //data fetch krke layega or yha fill kr degi dr me
+            OleDbCommand com = new OleDbCommand(sqlcmd, con);
+            OleDbDataReader dr = com.ExecuteReader(); 
+            return dr;
+        }
+        public  DataTable getTable(String sqlcmd, OleDbConnection con)
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter dapt = new OleDbDataAdapter(sqlcmd,con);
+            dapt.Fill(dt);
+            return (dt);
+        }
+    }
+}
