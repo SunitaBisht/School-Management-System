771305c725d5d65e166374d708a0e392da4e1caf
Sunita bisht <49061482+SunitaBisht@users.noreply.github.com>
2019-09-01 21:11:23 +0530

    After Login

---
+++ AfterLogin.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SchoolManagement
+{
+    partial class AfterLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.transctionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.feesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.studentUpdateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staffUpdateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.newStudentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.staffToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addSubjectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createAccountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.transctionToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1010, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // transctionToolStripMenuItem
+            // 
+            this.transctionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.feesToolStripMenuItem});
+            this.transctionToolStripMenuItem.Name = "transctionToolStripMenuItem";
+            this.transctionToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.transctionToolStripMenuItem.Text = "Transcation ";
+            // 
+            // feesToolStripMenuItem
+            // 
+            this.feesToolStripMenuItem.Name = "feesToolStripMenuItem";
+            this.feesToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.feesToolStripMenuItem.Text = "Fees";
+            this.feesToolStripMenuItem.Click += new System.EventHandler(this.feesToolStripMenuItem_Click);
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.studentUpdateToolStripMenuItem,
+            this.staffUpdateToolStripMenuItem});
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // studentUpdateToolStripMenuItem
+            // 
+            this.studentUpdateToolStripMenuItem.Name = "studentUpdateToolStripMenuItem";
+            this.studentUpdateToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.studentUpdateToolStripMenuItem.Text = "Student Detail";
+            this.studentUpdateToolStripMenuItem.Click += new System.EventHandler(this.studentUpdateToolStripMenuItem_Click);
+            // 
+            // staffUpdateToolStripMenuItem
+            // 
+            this.staffUpdateToolStripMenuItem.Name = "staffUpdateToolStripMenuItem";
+            this.staffUpdateToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.staffUpdateToolStripMenuItem.Text = "Staff Detail";
+            this.staffUpdateToolStripMenuItem.Click += new System.EventHandler(this.staffUpdateToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newStudentToolStripMenuItem,
+            this.staffToolStripMenuItem,
+            this.addSubjectToolStripMenuItem,
+            this.createAccountToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(43, 20);
+            this.toolStripMenuItem1.Text = "New";
+            // 
+            // newStudentToolStripMenuItem
+            // 
+            this.newStudentToolStripMenuItem.Name = "newStudentToolStripMenuItem";
+            this.newStudentToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.newStudentToolStripMenuItem.Text = "New Student";
+            this.newStudentToolStripMenuItem.Click += new System.EventHandler(this.newStudentToolStripMenuItem_Click);
+            // 
+            // staffToolStripMenuItem
+            // 
+            this.staffToolStripMenuItem.Name = "staffToolStripMenuItem";
+            this.staffToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.staffToolStripMenuItem.Text = "Staff";
+            this.staffToolStripMenuItem.Click += new System.EventHandler(this.staffToolStripMenuItem_Click);
+            // 
+            // addSubjectToolStripMenuItem
+            // 
+            this.addSubjectToolStripMenuItem.Name = "addSubjectToolStripMenuItem";
+            this.addSubjectToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.addSubjectToolStripMenuItem.Text = "Add Subject";
+            this.addSubjectToolStripMenuItem.Click += new System.EventHandler(this.addSubjectToolStripMenuItem_Click);
+            // 
+            // createAccountToolStripMenuItem
+            // 
+            this.createAccountToolStripMenuItem.Name = "createAccountToolStripMenuItem";
+            this.createAccountToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.createAccountToolStripMenuItem.Text = "Create Account";
+            this.createAccountToolStripMenuItem.Click += new System.EventHandler(this.createAccountToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // AfterLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1010, 492);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AfterLogin";
+            this.Text = "AfterLogin";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem newStudentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staffToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addSubjectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transctionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem feesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem studentUpdateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem staffUpdateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createAccountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
---
+++ AfterLogin.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManagement
+{
+    public partial class AfterLogin : Form
+    {
+        public AfterLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void newStudentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 ob = new Form1();                               //create object 
+            ob.MdiParent = this;
+            ob.Show();
+
+        }
+
+        private void staffToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FacultyRegi ob = new FacultyRegi();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void addSubjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddnewSubject ob = new AddnewSubject();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void feesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Feesubmit ob = new Feesubmit();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void studentUpdateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1Updates ob = new Form1Updates();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void staffUpdateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FacultyRegiUPDATE ob = new FacultyRegiUPDATE();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void createAccountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            creat ob = new creat();
+            ob.MdiParent = this;
+            ob.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
